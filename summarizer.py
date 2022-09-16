b03e2575899b0c96a3a6adcf28c71a70e61e8ed4
andreafancellu <andreafancellu123@gmail.com>
2022-09-16 18:04:19 +0200

    NASARI vectors from title

--- summarizer.py
+++ summarizer.py
@@ -9,18 +9,25 @@
 
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 
 CUE_PATH = 'data/cue_sentences.txt'
+NASARI_PATH_1 = 'data/mini_NASARI.tsv'
+NASARI_PATH_2 = 'data/dd-small-nasari-15.txt'
 
 def split_dot(text):
     return text.split('.')
 
-def remove_stopwords(text):
-    return [word for word in text if word not in nltk.corpus.stopwords.words('english')]
+def remove_stopwords(tokens):
+    return [lemmatize(word) for word in tokens if word not in nltk.corpus.stopwords.words('english')]
 
-def tokenize(text):
+def tokenize_simple(text):
     return word_tokenize(text)
+
+def tokenize_all(text):  # remove the punctuation too
+    tokenizer = RegexpTokenizer(r'\w+')
+    return tokenizer.tokenize(text)
 
 def lemmatize(word):    
     return WordNetLemmatizer().lemmatize(word)
@@ -32,10 +39,10 @@ def pos(tokens): #! not used
 def find_title_sentences(text, title): 
     sentences = []
     flag = False
-    text = ' '.join(remove_stopwords(tokenize(text)))
-    context_title = [lemmatize(word) for word in tokenize(title)]
-    for sentence in split_dot(text):
-        for word in tokenize(sentence):
+    text = ' '.join(remove_stopwords(tokenize_simple(text)))
+    context_title = [lemmatize(word.lower()) for word in tokenize_simple(title)]
+    for sentence in split_dot(text.lower()):
+        for word in tokenize_simple(sentence):
             if word in context_title and not flag: # still haven't found a word in the title for this sentence
                 sentences.append(sentence)
                 flag = True
@@ -47,19 +54,32 @@ def get_cue_sentences(path):
     cues = []
     with open(path, 'r') as f:
         for line in f:
-            cues.append(line)
+            cues.append(line.lower())
     return cues
 
 # finds relevant sentences
 def find_relevant_sentences(text):
     sentences = []
-    text = ' '.join(remove_stopwords(tokenize(text)))
+    text = ' '.join(remove_stopwords(tokenize_simple(text)))
     cues = get_cue_sentences(CUE_PATH)
     for sentence in split_dot(text):
         for c in cues:
-            if c in sentence:
+            if c in sentence.lower():
                 sentences.append(sentence)
     return sentences
 
+# get the nasari vectors associated with the tokens in title
+def get_nasari_from_title(text):
+    tokens = remove_stopwords(tokenize_all(text))
+    nasari = {}
+    with open(NASARI_PATH_2, 'r', encoding="utf8") as f:
+        for line in f:
+            for token in tokens:
+                if token.lower() == line.split(';')[1].lower():
+                    nasari[token] = line.split(';')[2:]
+    return nasari                         
+
 def overlap(list_1, list_2):
     return [value for value in list_1 if value in list_2]
+
+print(get_nasari_from_title('rock music over the years'))
